f86ee082a9ae73cc10041dfa29f3e2de439a4f33
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-02-26 17:21:33 -0300

    Add Knife configuration

---
+++ .chef/knife.rb
@@ -0,0 +1,15 @@
+chef_dir        = "#{ENV['HOME']}/.chef"
+
+log_level       :info
+log_location    STDOUT
+
+cache_type      "BasicFile"
+cache_options   :path => "#{chef_dir}/checksums"
+
+# EC2
+knife[:aws_access_key_id]     = ENV['AWS_ACCESS_KEY']
+knife[:aws_secret_access_key] = ENV['AWS_SECRET_KEY']
+knife[:aws_ssh_key_id]        = ENV['USER']
+knife[:region]                = "us-east-1"
+knife[:flavor]                = "m1.medium"
+knife[:image]                 = "ami-7ce17315"  # Debian 6.0.7 (Squeeze)
