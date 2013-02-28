6b8456723f2e8a4fd79028fa05037b24696ac355
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-02-27 23:42:00 -0300

    Add cookbook skeleton "debamine" for building AMIs

---
+++ site-cookbooks/debamine/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       'Teemu Matilainen'
+maintainer_email 'teemu.matilainen@iki.fi'
+license          'Apache v2.0'
+name             'debamine'
+description      'Installs ec2debian-build-ami and build an image'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports         'debian'
